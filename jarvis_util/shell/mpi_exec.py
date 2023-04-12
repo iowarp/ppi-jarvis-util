c1690c4c36d56a01e288e709569653ce5ae3f9bf
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-12 12:49:38 -0500

    Add hostfile as parmaeter to mpiexec

--- jarvis_util/shell/mpi_exec.py
+++ jarvis_util/shell/mpi_exec.py
@@ -2,10 +2,11 @@ from jarvis_util.shell.local_exec import LocalExec
 
 
 class MpiExec(LocalExec):
-    def __init__(self, cmd, nprocs=1, ppn=None, env=None):
+    def __init__(self, cmd, nprocs=1, ppn=None, hostfile=None, env=None):
         self.nprocs = nprocs
         self.ppn = ppn
         self.cmd = cmd
+        self.hostfile = None
         self.env = env
         if env is None:
             self.env = {}
@@ -15,6 +16,8 @@ class MpiExec(LocalExec):
         params = [f"mpirun -n {self.nprocs}"]
         if self.ppn is not None:
             params.append(f"-ppn {self.ppn}")
+        if self.hostfile is not None:
+            params.append(f"--hostfile {self.hostfile}")
         params += [f"-genv {key}={val}" for key, val in self.env.items()]
         params.append(self.cmd)
         cmd = " ".join(params)
