64f5d583a00de3e28c34419c787f14cac022ee21
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-17 06:46:38 -0500

    Remove -f pkill

--- jarvis_util/shell/kill.py
+++ jarvis_util/shell/kill.py
@@ -4,4 +4,4 @@ from .exec import Exec
 
 class Kill(Exec):
     def __init__(self, cmd, exec_info):
-        super().__init__(f"hostname && pkill {cmd}", exec_info)
+        super().__init__(f"pkill {cmd}", exec_info)
