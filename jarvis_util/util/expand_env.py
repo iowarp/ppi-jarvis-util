300d424ac4118519e8b4ec7faeeb381539b4396c
lukemartinlogan <lukemartinlogan@gmail.com>
2023-05-26 23:01:55 -0500

    Expand env

---
+++ jarvis_util/util/expand_env.py
@@ -0,0 +1,12 @@
+import os
+
+
+def expand_env(data):
+    if instance(data, str):
+        return os.path.expandvars(data)
+    if isinstance(data, dict):
+        for key, val in data.items():
+            data[key] = expand_env(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        for i, val in enumerate(data):
+            data[i] = expand_env(data)
