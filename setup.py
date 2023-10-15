5c5d8ed29b9001d73361473309f6d0e466734e19
jaime <jcernudagarcia@hawk.iit.edu>
2023-10-15 13:14:06 -0500

    updating the setup

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ import setuptools
 
 setuptools.setup(
     name="jarvis_util",
-    packages=setuptools.find_packages(),
-    scripts=['bin/pylsblk'],
     version="0.0.1",
+    description='Utilities to aid with creating shell scripts within Python.',\
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Luke Logan",
     author_email="llogan@hawk.iit.edu",
-    description="Various wrappers around shell utilities",
     url="https://github.com/scs-lab/jarvis-util",
-    classifiers = [
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyyaml',
+        'pylint==2.15.0',
+        'coverage==5.5',
+        'coverage-lcov==0.2.4',
+        'pytest==6.2.5',
+        'tabulate'
+    ],
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Development Status :: 0 - Pre-Alpha",
@@ -20,5 +29,4 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Application Configuration",
     ],
-    long_description=""
 )
