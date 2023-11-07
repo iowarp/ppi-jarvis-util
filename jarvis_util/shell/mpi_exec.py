e589dfd28f8f02647f3c8660ef1e96625d882976
jaime <jcernudagarcia@hawk.iit.edu>
2023-11-07 01:16:41 -0600

    forgot to delete the old version

--- jarvis_util/shell/mpi_exec.py
+++ jarvis_util/shell/mpi_exec.py
@@ -87,24 +87,6 @@ class MpichExec(LocalMpiExec):
     """
 
     def mpicmd(self):
-        params = [f'mpiexec -n {self.nprocs}']
-        if self.ppn is not None:
-            params.append(f'-ppn {self.ppn}')
-        if len(self.hostfile):
-            if self.hostfile.is_subset() or self.hostfile.path is None:
-                params.append(f'--host {",".join(self.hostfile.hosts)}')
-            else:
-                params.append(f'--hostfile {self.hostfile.path}')
-        params += [f'-genv {key}=\"{val}\"'
-                   for key, val in self.mpi_env.items()]
-        params.append(self.cmd)
-        cmd = ' '.join(params)
-        jutil = JutilManager.get_instance()
-        if jutil.debug_mpi_exec:
-            print(cmd)
-        return cmd
-
-    def mpicmd(self):
         params = ['mpiexec']
 
         if self.ppn is not None:
